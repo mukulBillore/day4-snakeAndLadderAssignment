23e18e0b53cda1d016f3232854be1dfd4b3848d7
mukulBillore <mukulbillore88@gmail.com>
2021-12-11 00:41:48 +0530

    updated sucussfull: the moves are added

--- App.java
+++ App.java
@@ -5,11 +5,24 @@ import java.util.Random;
 public class App {
 	public static void ForPlayerOne() {
 		System.out.println("Welocme to snake and ladder game for single player!!!");
-	//@use case1 
+		// @use case2
 		Random rand = new Random();
-	    int dice = rand.nextInt(7);
-	    System.out.println("the number on dice is :-"+dice);
+		int dice = rand.nextInt(7) + 1;
+		System.out.println("the number on dice is :-" + dice);
+		// @use case3
+		int p1Position = 0;
+		int moveTodo = rand.nextInt(3);
+		if (moveTodo == 0) {
+			p1Position = p1Position + dice; // ladder
+			System.out.println("hurry!! its the ladder!! so the position of the player is :"+p1Position);
+		} else if (moveTodo == 1) {
+			p1Position=p1Position-dice; // snake
+			System.out.println("sorry !! its a snake!! so the position of the player is :"+p1Position);
+		}
+		else 
+	    System.out.println("it is the waiting move !! so the position of the player is :"+p1Position);
 	}
+
 	public static void main(String[] args) {
 		App.ForPlayerOne();
 	}
