77bf92b61bf9590179a3ff800d5a2202d403728b
mukulBillore <mukulbillore88@gmail.com>
2021-12-11 00:13:43 +0530

    initial commit : susscussfully commited

---
+++ App.java
@@ -0,0 +1,13 @@
+package snakeAndLadderGame;
+
+public class App {
+	public static void ForPlayerOne() {
+		System.out.println("Welocme to snake and ladder game for single player!!!");
+	
+	}
+	public static void main(String[] args) {
+	App.ForPlayerOne();	
+
+	}
+
+}
