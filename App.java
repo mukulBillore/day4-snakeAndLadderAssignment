dd89ad60821adeaec97e81cd8602eca4710a738d
mukulBillore <mukulbillore88@gmail.com>
2021-12-11 00:21:19 +0530

    updated sucussful : dice is added

--- App.java
+++ App.java
@@ -1,13 +1,17 @@
 package snakeAndLadderGame;
 
+import java.util.Random;
+
 public class App {
 	public static void ForPlayerOne() {
 		System.out.println("Welocme to snake and ladder game for single player!!!");
-	
+	//@use case1 
+		Random rand = new Random();
+	    int dice = rand.nextInt(7);
+	    System.out.println("the number on dice is :-"+dice);
 	}
 	public static void main(String[] args) {
-	App.ForPlayerOne();	
-
+		App.ForPlayerOne();
 	}
 
 }
