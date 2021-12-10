972a0e1434f8c21170827d8d95157e4cb944b757
mukulBillore <mukulbillore88@gmail.com>
2021-12-11 00:48:13 +0530

    modified sucussfully : now position shoudn't be negative

--- App.java
+++ App.java
@@ -14,13 +14,17 @@ public class App {
 		int moveTodo = rand.nextInt(3);
 		if (moveTodo == 0) {
 			p1Position = p1Position + dice; // ladder
-			System.out.println("hurry!! its the ladder!! so the position of the player is :"+p1Position);
+			System.out.println("hurry!! its the ladder!! so the position of the player is :" + p1Position);
 		} else if (moveTodo == 1) {
-			p1Position=p1Position-dice; // snake
-			System.out.println("sorry !! its a snake!! so the position of the player is :"+p1Position);
-		}
-		else 
-	    System.out.println("it is the waiting move !! so the position of the player is :"+p1Position);
+			// @use case 4 if position is below zero then restarts from zero
+
+			p1Position = p1Position - dice; // snake
+			if (p1Position <= 0) {
+				p1Position = 0;
+			}
+			System.out.println("sorry !! its a snake!! so the position of the player is :" + p1Position);
+		} else
+			System.out.println("it is the waiting move !! so the position of the player is :" + p1Position);
 	}
 
 	public static void main(String[] args) {
